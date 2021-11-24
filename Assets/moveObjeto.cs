2d3f57f657fe9bd8fac5dde7b3334b3cb8ea246d
Infinitemarcus <marcus.natrielli@outlook.com>
2021-11-24 17:50:18 -0300

    Adicionando animação do bastão, fisica na bola e nas caixas do mapa e botão para reiniciar fase

---
+++ Assets/moveObjeto.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveObjeto : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    private bool moved;
+    Animator m_Animator;
+
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    private void Start()
+    {
+        moved = false;
+        m_Animator = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+       if(Input.GetKeyDown("z") && moved == false)
+        {
+            moved = true;
+
+            if (!m_Animator.GetBool("Activate"))
+            {
+                m_Animator.SetBool("Activate", true);
+            }
+       }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float force = 15000;
+
+        Vector3 dir = collision.contacts[0].point - transform.position;
+        dir = -dir.normalized;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(dir*force);
+    }
+}
