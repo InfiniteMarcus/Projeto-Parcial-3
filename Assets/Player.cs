6f689c07c524a3b6bda1d355e4812dda41e6d2ec
Lucas Martins <kim.dh@hotmail.com>
2021-11-24 18:09:32 -0300

    PPG4: Adição de mais uma câmera, inserção de projeção ortográfica. Interações troca de camera.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,11 +6,25 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
 
+    public Camera cam1;
+    public Camera cam2;
+
+    private void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("r"))
             SceneManager.LoadScene("SampleScene");
+
+        if (Input.GetKeyDown("x"))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
     }
 }
